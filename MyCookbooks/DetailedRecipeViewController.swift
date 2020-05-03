b0f81a2187f64ff730d0b0ce92a744d0c3062933
russperlow <rep4975@rit.edu>
2020-05-03 13:59:42 -0400

    delete button added and implemented

--- MyCookbooks/DetailedRecipeViewController.swift
+++ MyCookbooks/DetailedRecipeViewController.swift
@@ -16,6 +16,23 @@ class DetailedRecipeViewController: UIViewController {
     @IBOutlet weak var stepsTextField: UITextView!
     @IBOutlet weak var ingredientsTextField: UITextView!
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func trashButtonClicked(_ sender: Any) {
+        let alert = UIAlertController(title: "Delete Confirmation", message: "Are you sure you would like to delete \(recipe?.title)?", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Delete", style: UIAlertAction.Style.destructive, handler: { (UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+            self.deleteRecipe()
+
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func deleteRecipe(){
+        RecipeDb.sharedInstance.deleteRecipe(id: self.recipe!.id)
+        navigationController?.popViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         recipeTitle.text = recipe!.title
@@ -27,74 +44,5 @@ class DetailedRecipeViewController: UIViewController {
         stepsTextField.text = recipe!.steps
         imageView.image = ImageResourceManager.sharedInstance.getSavedImage(name: recipe!.title)
     
-        // Do any additional setup after loading the view.
     }
-    
-    func setRecipe(_recipe: Recipe){
-        self.recipe = _recipe
-        //recipeTitle.text = recipe!.title
-//        ingredientsTextField.layer.borderColor = UIColor.lightGray.cgColor
-//        ingredientsTextField.layer.borderWidth = 1.0
-//        ingredientsTextField.text = recipe!.ingredients
-//        stepsTextField.layer.borderColor = UIColor.lightGray.cgColor
-//        stepsTextField.layer.borderWidth = 1.0
-//        stepsTextField.text = recipe!.steps
-    }
-    
-    
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 4
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-    
-    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        var cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier")
-//
-//        if cell == nil {
-//            cell = UITableViewCell(style: .default, reuseIdentifier: "reuseIdentifier")
-//        }
-//        
-//        
-//        switch indexPath.section{
-//        case 0:
-//            cell!.textLabel?.text = recipe.title
-//        case 1:
-//            cell!.textLabel?.text = recipe.ingredients
-//            cell!.textLabel?.numberOfLines = 0
-//        case 2:
-//            cell!.textLabel?.text = recipe.steps
-//            cell!.textLabel?.numberOfLines = 0
-//        case 3:
-//            cell?.imageView?.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
-//            cell!.imageView?.image = ImageResourceManager.sharedInstance.getSavedImage(name: recipe.title)
-//        default:
-//            break;
-//        }
-//        return cell!
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        
-//        var title = ""
-//        
-//        switch section {
-//        case 0:
-//            title = "Title: "
-//        case 1:
-//            title = "Ingredients: "
-//        case 2:
-//            title = "Steps: "
-//        case 3:
-//            title = ""
-//        default:
-//            break;
-//        }
-//        
-//        return title
-//    }
-
 }
